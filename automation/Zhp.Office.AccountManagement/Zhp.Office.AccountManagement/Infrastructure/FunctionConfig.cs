518619d1f15cd7ad899e7bf3006b458bfd75a020
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2020-12-03 21:03:40 +0100

    Add stub for migrator Azure function

---
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Infrastructure/FunctionConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zhp.Office.AccountManagement.Infrastructure
+{
+    internal class FunctionConfig
+    {
+        public JiraConfig Jira { get; private set; } = new JiraConfig();
+        
+        internal class JiraConfig
+        {
+            public string JiraUri { get; private set; } = string.Empty;
+            public string ConsumerKey { get; private set; } = string.Empty;
+            public string ConsumerSecret { get; private set; } = string.Empty;
+            public string OAuthAccessToken { get; private set; } = string.Empty;
+            public string OAuthTokenSecret { get; private set; } = string.Empty;
+        }
+    }
+}
---
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Infrastructure/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Atlassian.Jira;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Zhp.Office.AccountManagement.Infrastructure
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var s = builder.Services;
+            
+            s.AddSingleton(CreateJiraClient);
+            s.AddSingleton(LoadConfig);
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            base.ConfigureAppConfiguration(builder);
+
+            builder.ConfigurationBuilder.SetBasePath(builder.GetContext().ApplicationRootPath)
+                .AddJsonFile("appsettings.json");
+        }
+
+        private FunctionConfig LoadConfig(IServiceProvider c) => 
+            c.GetRequiredService<IConfiguration>()
+                .Get<FunctionConfig>(o => o.BindNonPublicProperties = true);
+
+        private Jira CreateJiraClient(IServiceProvider c)
+        {
+            var config = c.GetRequiredService<FunctionConfig>().Jira;
+            return Jira.CreateOAuthRestClient(
+                config.JiraUri,
+                config.ConsumerKey,
+                config.ConsumerSecret,
+                config.OAuthAccessToken,
+                config.OAuthTokenSecret);
+        }
+    }
+}
