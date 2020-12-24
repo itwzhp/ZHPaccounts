c08f14571313c4730793a357d8dad06a3df915a1
norberto5 <admin@norberto5.pl>
2020-12-24 01:20:38 +0100

    Split PasswordGeneratorTests for Azure AD Password Policy tests and current implementation tests

--- automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement.Tests/Domain/Services/PasswordGeneratorTests.cs
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement.Tests/Domain/Services/PasswordGeneratorTests.cs
@@ -9,9 +9,6 @@ using Zhp.Office.AccountManagement.Domain.Services;
 
 namespace Zhp.Office.AccountManagement.Tests.Domain.Services
 {
-    /// <summary>
-    /// Requirements based on https://docs.microsoft.com/en-us/azure/active-directory/authentication/concept-sspr-policy#password-policies-that-only-apply-to-cloud-user-accounts
-    /// </summary>
     [SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "Required for Repeat attribute")]
     public class PasswordGeneratorTests : IDisposable
     {
@@ -26,12 +23,15 @@ namespace Zhp.Office.AccountManagement.Tests.Domain.Services
         public void Dispose()
             => generator.Dispose();
 
+        // Requirements based on https://docs.microsoft.com/en-us/azure/active-directory/authentication/concept-sspr-policy#password-policies-that-only-apply-to-cloud-user-accounts
+        #region Microsoft Azure AD Password policy requirements
+
         [Theory, Repeat(50)]
-        public void PasswordShouldHaveValidLength(int runNumber)
-            => subject.GeneratePassword().Should().HaveLength(8);
+        public void AzureADPolicy_PasswordShouldHaveValidLength(int runNumber)
+            => subject.GeneratePassword().Length.Should().BeGreaterOrEqualTo(8).And.BeLessOrEqualTo(256);
 
         [Theory, Repeat(200)]
-        public void PasswordShouldHave3Of3CharGroups(int runNumber)
+        public void AzureADPolicy_PasswordShouldHave3Of4CharGroups(int runNumber)
         {
             var basicGroups = new[] { UnicodeCategory.LowercaseLetter, UnicodeCategory.UppercaseLetter, UnicodeCategory.DecimalDigitNumber };
 
@@ -42,21 +42,45 @@ namespace Zhp.Office.AccountManagement.Tests.Domain.Services
             var foundGroups = basicGroups.Count(g => charGroups.Contains(g))
                             + (charGroups.Any(g => !basicGroups.Contains(g) && g != char.GetUnicodeCategory(' ')) ? 1 : 0);
 
-            foundGroups.Should().Be(3, $"Password {password} is too simple");
+            foundGroups.Should().BeGreaterOrEqualTo(3, $"Password {password} is too simple");
+        }
+
+        [Theory, Repeat(200)]
+        public void AzureADPolicy_PasswordsShouldContainOnlyAllowedChars(int runNumber)
+            => subject.GeneratePassword().Should().MatchRegex(@"^[a-zA-Z0-9 @#$%^&*\-_!+=\[\]{}|\\:',\.?/`~""();]*$");
+
+        [Theory, Repeat(50)]
+        public void AzureADPolicy_PasswordsShouldNotContainUnicode(int runNumber)
+            => subject.GeneratePassword().AsEnumerable().Should().NotContain(c => c > 255);
+
+        #endregion
+
+        #region Current implementation tests
+
+        [Theory, Repeat(50)]
+        public void PasswordShouldHaveValidLength(int runNumber)
+            => subject.GeneratePassword().Should().HaveLength(8);
+
+        [Theory, Repeat(200)]
+        public void PasswordShouldContainDigitLowerAndUpperLetter(int runNumber)
+        {
+            string password = subject.GeneratePassword();
+
+            bool isPasswordStrong = password.Any(char.IsDigit) && password.Any(char.IsUpper) && password.Any(char.IsLower);
+
+            isPasswordStrong.Should().BeTrue($"Password {password} is too simple");
         }
 
         [Theory, Repeat(200)]
         public void PasswordsShouldContainOnlyAllowedChars(int runNumber)
             => subject.GeneratePassword().Should().MatchRegex(@"^[a-zA-Z0-9]*$");
 
-        [Theory, Repeat(50)]
-        public void PasswordsShouldNotContainUnicode(int runNumber)
-            => subject.GeneratePassword().AsEnumerable().Should().NotContain(c => c > 255);
-
         [Fact]
         public void PasswordsShouldDiffer()
             => Enumerable.Range(1, 200)
             .Select(_ => subject.GeneratePassword())
             .Should().OnlyHaveUniqueItems();
+
+        #endregion
     }
 }
