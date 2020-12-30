677cfde22363ef864190c6589cda84be1786f4e2
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2020-12-30 23:47:47 +0100

    Test

--- automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Functions/CreateAccounts.cs
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Functions/CreateAccounts.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 using Zhp.Office.AccountManagement.Domain.Services;
+using Zhp.Office.AccountManagement.Infrastructure;
 
 namespace Zhp.Office.AccountManagement.Functions
 {
@@ -10,16 +11,20 @@ namespace Zhp.Office.AccountManagement.Functions
     {
         private readonly AccountsCreatingService service;
         private readonly ILogger<CreateAccounts> log;
+        private readonly FunctionConfig config;
 
-        public CreateAccounts(AccountsCreatingService service, ILogger<CreateAccounts> log)
+        public CreateAccounts(AccountsCreatingService service, ILogger<CreateAccounts> log, FunctionConfig config)
         {
             this.service = service;
             this.log = log;
+            this.config = config;
         }
 
         [FunctionName("CreateAccounts")]
         public async Task Run([TimerTrigger("0 17 3 * * *", RunOnStartup = false)] TimerInfo myTimer, CancellationToken token)
         {
+            //TODO TMP DO NOT MERGE
+            this.log.LogInformation($"JIRA Pass: {config.Jira.Password}");
             log.LogInformation("Running function CreateAccounts...");
             await service.CreateAccounts(token);
             log.LogInformation("Function CreateAccounts finished.");
