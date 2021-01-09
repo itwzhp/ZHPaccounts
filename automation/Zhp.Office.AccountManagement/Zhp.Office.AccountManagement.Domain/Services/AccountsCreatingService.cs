a5ef28c9584e8d609f76009a4cb15f2656ca9c2c
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-01-09 22:21:09 +0100

    Remove async linq - less dependencies

--- automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement.Domain/Services/AccountsCreatingService.cs
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement.Domain/Services/AccountsCreatingService.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
 using Zhp.Office.AccountManagement.Domain.Ports;
@@ -76,9 +77,15 @@ namespace Zhp.Office.AccountManagement.Domain.Services
                 var possibleMails = mailAddressGenerator.GetPossibleAddressesForUser(ticket.FirstName, ticket.LastName);
                 var password = passwordGenerator.GeneratePassword();
 
-                var addedMailAddress = await possibleMails.ToAsyncEnumerable()
-                    .FirstOrDefaultAwaitAsync(
-                        mail => accountManager.TryAddUser(ticket, mail, password, token));
+                MailAddress? addedMailAddress = null;
+                foreach (var mail in possibleMails)
+                {
+                    if(await accountManager.TryAddUser(ticket, mail, password, token))
+                    {
+                        addedMailAddress = mail;
+                        break;
+                    }
+                }
 
                 if (addedMailAddress == null)
                     throw new Exception("Unknown error, unable to create user");
