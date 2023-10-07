91394bd35ff4a2f9f05e013859b3a3c463596f68
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2023-10-07 14:47:15 +0200

    pr

--- automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Adapters/ActiveDirectory/AzureActiveDirectoryClient.cs
+++ automation/Zhp.Office.AccountManagement/Zhp.Office.AccountManagement/Adapters/ActiveDirectory/AzureActiveDirectoryClient.cs
@@ -70,7 +70,7 @@ namespace Zhp.Office.AccountManagement.Adapters.ActiveDirectory
                 UsageLocation = "PL",
 
                 EmployeeId = request.MembershipNumber,
-                EmployeeType = "Tipi",
+                EmployeeType = "Tipi-ticket",
             };
 
             var licenses = new[] { new AssignedLicense { SkuId = new Guid(activeDirectoryConfig.DefaultLicenseSku) } };
